bc738381ba3984d71477b5f5777c2a0a516a6045
datapsyche <jithinjkumar@gmail.com>
2020-02-15 15:38:37 +0530

    changed django admin

--- ticket/urls.py
+++ ticket/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+admin.site.site_header = 'CTSS Administration'
+admin.site.index_title = 'Advanced Features and Options'
+admin.site.site_title = 'Centralized Transparent Service System Administration'
+
+
+
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('addticket', views.addticket, name="addticket"),
-    # path('adddepartment', views.adddepartment, name="adddepartment"),
-    # path('addfollowup', views.addfollowup, name="addfollowup"),
     path('addmarriagecertificate', views.addmarriagecertificate, name="addmarriagecertificate"),
     path('addbirthcertificate', views.addbirthcertificate, name="addbirthcertificate"),
     path('ticket/<int:num>/', views.ticketinfo, name="showticketinfo"),
